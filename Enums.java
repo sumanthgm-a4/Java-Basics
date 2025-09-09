dd2605a91ad4df59b6a87bd67be030ad6e2fe8e3
sum <214g1a32a4@srit.ac.in>
2025-09-09 21:50:23 +0530

    Added Enums.java

---
+++ Enums.java
@@ -0,0 +1,10 @@
+public class Enums {
+    public static void main(String[] args) {
+        System.out.println(Days.Monday);
+    }
+}
+
+// Enums are a special type of classes used to define constants
+enum Days {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
